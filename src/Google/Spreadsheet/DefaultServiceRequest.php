72fc5ebd348fe4c4d5c914b0447423aa32563279
lemats <mhayoz@c13229.local>
2015-01-19 16:41:23 +0100

    Add 404 http code to handle 'you need permission' exception.

--- src/Google/Spreadsheet/DefaultServiceRequest.php
+++ src/Google/Spreadsheet/DefaultServiceRequest.php
@@ -227,11 +227,16 @@ class DefaultServiceRequest implements ServiceRequestInterface
         $info = curl_getinfo($ch);
         $httpCode = (int)$info['http_code'];
 
-        if($httpCode > 299) {
-            if($httpCode === 401) {
-                throw new UnauthorizedException('Access token is invalid', 401);
-            } else {
-                throw new Exception('Error in Google Request', $info['http_code']);
+        if ($httpCode > 299) {
+            switch ($httpCode) {
+                case 401:
+                    throw new UnauthorizedException('Access token is invalid', 401);
+                    break;
+                case 404:
+                    throw new UnauthorizedException('You need permission', 404);
+                    break;
+                default:
+                    throw new Exception('Error in Google Request', $info['http_code']);
             }
         }
 

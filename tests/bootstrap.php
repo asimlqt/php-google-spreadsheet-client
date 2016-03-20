090e3be7183924596ec02c05894f6503b129f419
Asim Liaquat <asimlqt22@gmail.com>
2016-03-20 00:29:19 +0000

    Removed commented out code from the test bootstrap script

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,3 @@
 <?php
 
-// ini_set('display_errors', 1);
-// error_reporting(E_ALL);
-
-// $srcDir = realpath(__DIR__ . '/../src/');
-
-// set_include_path($srcDir . PATH_SEPARATOR . get_include_path());
-
-// spl_autoload_register(function ($class) {
-//     if(strpos($class, '\\') !== false) {
-//         include str_replace("\\", "/", $class) . '.php';
-//     }
-// });
+// bootstrap
